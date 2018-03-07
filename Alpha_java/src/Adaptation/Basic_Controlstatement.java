2917eb2bc78841d24638461528fdb352eda0efc9
cogito1016 <cogito1016@naver.com>
2018-03-07 18:09:03 +0900

    1.Changed choice environment in Basic_Controlstatement class

--- Alpha_java/src/Adaptation/Basic_Controlstatement.java
+++ Alpha_java/src/Adaptation/Basic_Controlstatement.java
@@ -12,23 +12,40 @@ class Basic_Controlstatement {
 				   else
 				   {sentence2;}
 	*/
+	private int num=0;
 	private int grade;
 	Scanner input = new Scanner(System.in);
 	
+	//this is Constructor in this class
 	Basic_Controlstatement() {
-		this.in_num();
+		this.in_num_SW();
 	}
+	
+	//Conditional construction SWITCH
+	private void in_num_SW() {
+		System.out.print("Choice the topic (1:IF-ELSE 2:WHILE 3:FOR 4:BREAK)");
+		num = input.nextInt();
+		//THis is SWITCH
+		switch(num)
+		{
+		case 1:
+			this.in_num_IF();
+			break;
+		}
+		
+	}	
 	   
-	private void in_num() {
+	//Conditional construction IF_ELSE
+	private void in_num_IF() {
 		System.out.print("Input your grade");
 		grade = input.nextInt();
-		/**/
-		//This is example of conditional construction.
+		//This is IF-ELSE
 		if(grade>=60)
 		{System.out.println("you possible to receive money");}
 		else
 		{System.out.println("you impossible to receive money");}
 	}	
 	
+
 
 }
