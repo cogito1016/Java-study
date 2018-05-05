db089970588d3952ad3f2dd7b057a693ed374d86
cogito1016 <cogito1016@naver.com>
2018-05-05 19:09:41 +0900

    (Report_Compare) make a structure(1)

--- Alpha_java/src/Adaptation/Report_Compare.java
+++ Alpha_java/src/Adaptation/Report_Compare.java
@@ -5,7 +5,16 @@ import java.util.*;
 import java.io.*;
 /*리포트 비교 프로그램 작성
  *최대한 교수님말대로 화일처리+컬렉션(Map)을 사용하여 구현해보도록 하겠습니다.
- **/
+ *다음과 같은 알고리즘으로 수행해보겠습니다.
+ *1. Read each report to file.
+ *2. Input each report to token, using 2 map.
+ *   **In map, each token must not overlap.**
+ *3. Then, I will have two map that have non-overlap token.
+ *4. Finally, I need two data.
+ *   -One is total word count(Map1.key + Map2.key)
+ *   -Two is Overlap word count(Map1.key && Map2.key)
+ *5. Final op = > OverlapWordCount/TotalWordCount*/
+
 
 public class Report_Compare {
 	File f1 = new File("News.txt");
@@ -23,5 +32,17 @@ public class Report_Compare {
 		
 		b1.close();
 		r1.close();
+	}
+	
+	void mainop()
+	{
+		while(sentence.hasNextLine())
+		{
+			String sv[] = sentence.nextLine().split(" ");
+			for(int i=0; i<sv.length;i++)
+			{
+				
+			}
+		}
 	}
 }
