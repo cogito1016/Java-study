9ab2671b0f3ced2741a0044842f462263ca651d5
cogito1016 <cogito1016@naver.com>
2022-06-17 20:27:46 +0900

    feat: Init Repeat

---
+++ src/java8/run/Repeat_220617.java
@@ -0,0 +1,15 @@
+package run;
+
+//실행어라운드
+//함수 디스크럽터 시그니처
+//@FunctionalInteface
+//동작파라미터화
+//Predicate
+//Consumer
+//Functiton
+
+public class Repeat_220617 {
+
+    public static void main(String[] args) {
+    }
+}
