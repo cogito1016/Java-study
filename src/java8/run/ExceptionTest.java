b9a2ff5d87b0fe90a0bbae7fab5e016879b98ce0
cogito1016 <cogito1016@naver.com>
2022-06-22 23:51:00 +0900

    feat: Exception2 - 함수형인터페이스에 선언하거나, try-catch사용

--- src/java8/run/ExceptionTest.java
+++ src/java8/run/ExceptionTest.java
@@ -3,6 +3,8 @@ package run;
 import processor.BufferedReaderProcessor;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.function.Function;
 
 public class ExceptionTest {
 
@@ -10,7 +12,15 @@ public class ExceptionTest {
 
         //예외를 위해
         //함수형 인터페이스에 직접 예외를 선언하거나,
-        //람다를 try/catch블록으로 감싸야한다.
         BufferedReaderProcessor p = (BufferedReader br)->br.readLine();
+
+        //람다를 try/catch블록으로 감싸야한다.
+        Function<BufferedReader, String> f = (BufferedReader b) -> {
+            try{
+                return b.readLine();
+            }catch (IOException e){
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
