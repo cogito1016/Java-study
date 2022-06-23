84389e2811ab1cd34e3000827c241d83f0c05830
cogito1016 <cogito1016@naver.com>
2022-06-23 19:45:53 +0900

    feat: String정렬

--- src/java8/run/MethodReferenceTest.java
+++ src/java8/run/MethodReferenceTest.java
@@ -4,11 +4,12 @@ import entity.Apple;
 import enums.Color;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class MethodReferenceTest {
-    public static void main(String[] args) {
+    public static void example_1(){
         Apple a1 = new Apple("400", Color.GREEN);
         Apple a2 = new Apple("200", Color.RED);
 
@@ -20,5 +21,19 @@ public class MethodReferenceTest {
         inventory.sort(Comparator.comparing(Apple::getWeight));
 
         Apple.printApples(inventory);
+    }
+
+    public static void example_2(){
+        List<String> str = Arrays.asList("a","b","c","d");
+        str.sort((s1, s2)->s1.compareToIgnoreCase(s2));
+
+        for(String s : str){
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+//        example_1();
+        example_2();
     }
 }
