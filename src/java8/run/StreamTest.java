0f98bed0f5c169f3fe4b55c20ec842fa28c38373
cogito1016 <cogito1016@naver.com>
2022-07-29 18:04:46 +0900

    refactor: Stream 처리

--- src/java8/run/StreamTest.java
+++ src/java8/run/StreamTest.java
@@ -6,15 +6,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamTest {
-    public static void main(String[] args) {
-        List<Dish> menu = new ArrayList<>();
-        Dish dish1 = new Dish(100,"Ham");
-        Dish dish2 = new Dish(500,"Cal");
-        menu.add(dish1);
-        menu.add(dish2);
 
+    public static void collectionProcess_1(List<Dish> menu){
         ArrayList<Dish> lowCaloricDishes = new ArrayList<>();
         for(Dish dish : menu){
             if(dish.getCalories() < 400){
@@ -35,5 +31,29 @@ public class StreamTest {
         }
 
         Dish.printDishList(lowCaloricDishes);
+    }
+
+    public static void collectionProcess_2(List<Dish> menu){
+        List<String> lowCaloricDishesName =
+                menu.stream()
+                        .filter(d->d.getCalories() < 400)
+                        .sorted(Comparator.comparing(Dish::getCalories))
+                        .map(Dish::getName)
+                        .collect(Collectors.toList());
+
+        for(String name : lowCaloricDishesName){
+            System.out.println(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Dish> menu = new ArrayList<>();
+        Dish dish1 = new Dish(100,"Ham");
+        Dish dish2 = new Dish(500,"Cal");
+        menu.add(dish1);
+        menu.add(dish2);
+
+//        collectionProcess_1(menu);
+        collectionProcess_2(menu);
     }
 }
