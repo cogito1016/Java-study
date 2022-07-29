5e4e8e774ff851c9c9933a562452bec4c45e3931
cogito1016 <cogito1016@naver.com>
2022-07-29 18:00:10 +0900

    feat: lowCaloricDishes는 가비지변수로만 쓰임

--- src/java8/entity/Dish.java
+++ src/java8/entity/Dish.java
@@ -1,9 +1,28 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class Dish {
     private int calories;
+    private String name;
+
+    public Dish(int calories, String name){
+        this.calories = calories;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 
     public int getCalories(){
         return calories;
+    }
+
+    public static void printDishList(ArrayList<Dish> dishes){
+        System.out.println("-----------------------");
+        for(Dish dish : dishes){
+            System.out.println("Name : " + dish.name + " Calories : " + dish.getCalories());
+        }
     }
 }
--- src/java8/run/StreamTest.java
+++ src/java8/run/StreamTest.java
@@ -3,18 +3,37 @@ package run;
 import entity.Dish;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamTest {
     public static void main(String[] args) {
         List<Dish> menu = new ArrayList<>();
+        Dish dish1 = new Dish(100,"Ham");
+        Dish dish2 = new Dish(500,"Cal");
+        menu.add(dish1);
+        menu.add(dish2);
 
-        List<Dish> lowCaloricDishes = new ArrayList<>();
+        ArrayList<Dish> lowCaloricDishes = new ArrayList<>();
         for(Dish dish : menu){
             if(dish.getCalories() < 400){
                 lowCaloricDishes.add(dish);
             }
         }
 
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish o1, Dish o2) {
+                return Integer.compare(o1.getCalories(), o2.getCalories());
+            }
+        });
+
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for(Dish dish: lowCaloricDishes){
+            lowCaloricDishesName.add(dish.getName());
+        }
+
+        Dish.printDishList(lowCaloricDishes);
     }
 }
