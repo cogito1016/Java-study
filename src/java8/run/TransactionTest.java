2c693d43a18b3111c500057d19c7367d02c4bf09
cogito1016 <cogito1016@naver.com>
2022-09-17 17:31:28 +0900

    feat: 문제털기~ *특이사항 max,min 시 REDUCE!!!:

--- src/java8/run/TransactionTest.java
+++ src/java8/run/TransactionTest.java
@@ -45,6 +45,58 @@ public class TransactionTest {
         System.out.println(result);
     }
 
+    public static void problem_4(List<Transaction> transactions){
+        //모든 거래자의 이름을 알파베순으로 정렬해서 반환
+        List<Trader> result =
+                transactions.stream()
+                        .map((element)->element.getTrader())
+                        .sorted(Comparator.comparing(Trader::getName))
+                        .distinct()
+                        .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+
+    public static void problem_5(List<Transaction> transactions){
+        //밀라노에 거래자있니
+
+        boolean result = transactions.stream()
+                .map((element)->element.getTrader())
+                .anyMatch((element)->element.getCity().equals("Milan"));
+
+        System.out.println(result);
+    }
+
+    public static void problem_6(List<Transaction> transactions){
+        //케임브리지에 거주하는 거래자의 모든 트랜잭션 값
+        List<Integer> result = transactions.stream()
+                .filter((element)->element.getTrader().getCity().equals("Cambridge"))
+                .map((element)->element.getValue())
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+
+    public static void problem_7(List<Transaction> transactions){
+        //전체 트랜잭션 중 최댓값
+        Integer result = transactions.stream()
+                .map((element)->element.getValue())
+                .reduce(Integer::max)
+                .get();
+
+        System.out.println(result);
+    }
+
+    public static void problem_8(List<Transaction> transactions){
+        //전체 트랜잭션 중 최소값
+        Integer result = transactions.stream()
+                .map((element)->element.getValue())
+                .reduce(Integer::min)
+                .get();
+
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         Trader raoul = new Trader("Raoul", "Cambridge");
         Trader mario = new Trader("Mario", "Milan");
@@ -62,6 +114,11 @@ public class TransactionTest {
 
 //        problem_1(transactions);
 //        problem_2(transactions);
-        problem_3(transactions);
+//        problem_3(transactions);
+//        problem_4(transactions);
+//        problem_5(transactions);
+//        problem_6(transactions);
+//        problem_7(transactions);
+        problem_8(transactions);
     }
 }
