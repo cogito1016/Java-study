dc41f0138713495d836b81027e8ace2ddbe41868
cogito1016 <cogito1016@naver.com>
2022-09-22 16:14:22 +0900

    feat: Sum은 기본값이 0, 근데 Max는? -> Optioanl의 사용

--- src/java8/run/StreamVariation.java
+++ src/java8/run/StreamVariation.java
@@ -5,6 +5,7 @@ import entity.Dish;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -25,6 +26,13 @@ public class StreamVariation {
         stream.forEach((element)-> System.out.println(element.toString()));
     }
 
+    public static void stream_ex_3(){
+        List<Dish> menu = new ArrayList<>();
+        OptionalInt optionalInt = menu.stream().mapToInt(Dish::getCalories).max();
+        int max = optionalInt.orElse(1);
+        System.out.println(max);
+    }
+
     public static void main(String[] args) {
         List<Dish> menu = Arrays.asList(
                 new Dish("Fish",false,100, Dish.Type.FISH),
@@ -32,7 +40,8 @@ public class StreamVariation {
         );
 
 //        stream_ex_1(menu);
-        stream_ex_2(menu);
+//        stream_ex_2(menu);
+        stream_ex_3();
     }
 
 }
