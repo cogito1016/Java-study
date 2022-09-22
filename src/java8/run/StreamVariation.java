c4a1a016ef2e77fcd1bbf5428d8c9ccc95c15b37
cogito1016 <cogito1016@naver.com>
2022-09-22 16:00:25 +0900

    feat: 기본형특화스트림 -> 객체스트림 변환(박싱)

--- src/java8/run/StreamVariation.java
+++ src/java8/run/StreamVariation.java
@@ -5,18 +5,34 @@ import entity.Dish;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamVariation {
-    public static void main(String[] args) {
-        List<Dish> menu = Arrays.asList(
-                new Dish("Fish",false,100, Dish.Type.FISH),
-                new Dish("Dog",false,500, Dish.Type.MEAT)
-        );
 
+    public static void stream_ex_1(List<Dish> menu){
         int calories = menu.stream()
                 .mapToInt(Dish::getCalories)
                 .sum();
 
         System.out.println(calories);
     }
+
+    public static void stream_ex_2(List<Dish> menu){
+        IntStream intStream = menu.stream().mapToInt(Dish::getCalories);
+        Stream<Integer> stream = intStream.boxed();
+
+        stream.forEach((element)-> System.out.println(element.toString()));
+    }
+
+    public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("Fish",false,100, Dish.Type.FISH),
+                new Dish("Dog",false,500, Dish.Type.MEAT)
+        );
+
+//        stream_ex_1(menu);
+        stream_ex_2(menu);
+    }
+
 }
