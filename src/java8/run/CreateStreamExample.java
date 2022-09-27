042d8496782d0919a9a97de65429da542ad7e5db
cogito1016 <cogito1016@naver.com>
2022-09-27 18:12:03 +0900

    feat: Null이 가능한 객체로 슽트림을 만들 때

--- src/java8/run/CreateStreamExample.java
+++ src/java8/run/CreateStreamExample.java
@@ -9,6 +9,14 @@ public class CreateStreamExample {
         stream.map(String::toUpperCase).forEach((element)-> System.out.println(element));
     }
 
+    //Null이 될 수 있는 객체를 스트림으로 만들기
+    public static void createNullableObjectStream(){
+        String s = System.getProperty("home");
+
+        //예전같으면..
+        Stream<String> stream = s==null?Stream.empty():Stream.of(s);
+    }
+
     public static void main(String[] args) {
         createValueStream();
     }
